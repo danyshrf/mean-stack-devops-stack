e018df8664d9611afdda4112301fcf5002aa1ccb
daanysh <93125871+danyshrf@users.noreply.github.com>
2026-02-24 12:44:15 +0530

    Update MongoDB configuration to use environment variable

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/*module.exports = {
   url: "mongodb://localhost:27017/dd_db"
+};
+*/
+module.exports = {
+  url: process.env.MONGO_URL || "mongodb://mongodb:27017/tutorials_db"
 };
